da08b4356c43dd9d174ea3c74c7e5863eb3e2171
drawotphub <Kashish.rudedude@gmail.com>
2025-05-02 11:54:12 +0000

    Fix Metro connection and process killing

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from './screens/Login';
-import RegisterScreen from './screens/Register';
-import DashboardScreen from './screens/Dashboard';
 
 const Stack = createStackNavigator();
 
@@ -12,8 +10,6 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Register" component={RegisterScreen} />
-        <Stack.Screen name="Dashboard" component={DashboardScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
